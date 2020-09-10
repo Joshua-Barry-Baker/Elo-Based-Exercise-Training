504f6c4ca4b3812d1de4940acc9554b0ebc3827e
Joshua-Barry-Baker <josh.barry.baker@googlemail.com>
2020-09-10 18:50:26 +0100

    Add files via upload
    fixed the exercise class

--- exercise.js
+++ exercise.js
@@ -1,36 +1,40 @@
-const createExercise = (name, reps, weight, ratioEMG) => {
-    return{
-        name,
-        reps: {
-            high,
-            mid,
-            low
-        },
-        weight: {
-            high,
-            mid,
-            low
-        },
-        ratioEMG: {
-            backUpper,
-            backLower,
-            glutes,
-            hams,
-            quads,
-            calfs,
-            chest,
-            biceps,
-            triceps,
-            forarms,
-            core
+class Exercise {
+    constructor(name, reps, weight, ratioEMG){
+        this.name = name;
+        //Reps
+//        console.log(reps.high);
+        this.reps = {
+            high: reps.high,
+            mid: reps.mid,
+            low: reps.low
+        }
+        //Weight
+        this.weight = {
+            high: weight.high,
+            mid: weight.mid,
+            low: weight.low
+        }
+        //ratioEMG
+        this.ratioEMG = {
+            backUpper: ratioEMG.backUpper,
+            backLower: ratioEMG.backUpper,
+            glutes: ratioEMG.glutes,
+            hams: ratioEMG.hams,
+            quads: ratioEMG.quads,
+            calfs: ratioEMG.calfs,
+            chest: ratioEMG.chest,
+            biceps: ratioEMG.biceps,
+            triceps: ratioEMG.triceps,
+            forarms: ratioEMG.forarms,
+            core: ratioEMG.core
         }
     }
 }
 let bodyWeight = 70;
-pressUps = createExercise(
+
+pressUps = new Exercise(
     'Press ups', 
-    {high: 14, mid: 8, low: 4 }, 
-    {high: bodyWeight*0.77, mid: bodyWeight*0.67, low: bodyWeight*0.57},
-    {glutes: 5, quads:5, core:10, chest:40, triceps:40}
-    );
+    {high:14, mid: 8, low: 4},
+    {high:bodyWeight*0.77, mid:bodyWeight*0.67, low:bodyWeight*0.57},
+    {chest:50, triceps:50});
 console.log(pressUps);
