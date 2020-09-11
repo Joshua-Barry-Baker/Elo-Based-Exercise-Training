156fda93868170df0b733b44d499356a8ca0bb93
Joshua-Barry-Baker <josh.barry.baker@googlemail.com>
2020-09-11 11:58:40 +0100

    Add files via upload

--- newELO.js
+++ newELO.js
@@ -1,14 +1,5 @@
 newELO = (a, b) => {
     expectedRatio = (1 /( 1 + 10^((a-b)/400)));
-    aNew = a*expectedRatio
+    aNew = a + a * expectedRatio;
     return aNew;
 };
-
-/* //Test
-let a = 2000;
-let b = 1000;
-
-console.log(`a = ${a} and b = ${b}`);
-a = newELO(a,b);
-console.log(`a new elo rating ${a}`);
-*/
