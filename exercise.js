8d70cac6f974160c97406a37378ddf829a156258
Joshua-Barry-Baker <josh.barry.baker@googlemail.com>
2020-09-16 14:14:13 +0100

    Display and small adjustments
    Added a basic display. The user still has to open the console to see the program work, however, the use of buttons to call functions has been added.
    
    Exercises have had their starting reps reduced so new users will not have to lose progress in order to start.
    
    The planDaly100 function no longer instantly updates the exercises. rather requires the user to log them.

--- exercise.js
+++ exercise.js
@@ -33,7 +33,7 @@ let bodyWeight = 70;
 
 let pressUps = new Exercise(
     'Press ups', 
-    {high:14, mid: 8, low: 4},
+    {high:5, mid: 3, low: 2},
     {high:Math.floor(bodyWeight*0.77), mid:Math.floor(bodyWeight*0.67), low:Math.floor(bodyWeight*0.57)},
     {chest:50, triceps:50});
 
@@ -41,15 +41,15 @@ console.log(pressUps);
 
 let sitUps = new Exercise(
     'Sit ups', 
-    {high:25, mid: 20, low: 15},
-    {high:bodyWeight*55.1, mid:bodyWeight*(55.1/1.33), low:bodyWeight*(0.57/2)},
+    {high:7, mid: 5, low: 3},
+    {high:bodyWeight*0.551, mid:bodyWeight*(0.551/1.33), low:bodyWeight*(0.57/2)},
     {core:100});
 
 console.log(sitUps);
 
 let squats = new Exercise(
     'Squats',
-    {high:14, mid:8, low:4},
+    {high:7, mid:5, low:3},
     {high:bodyWeight + 5, mid:bodyWeight + 3, low:bodyWeight},
     {quads:50, glutes:40, hams:10});
 
--- plans.js
+++ plans.js
@@ -2,64 +2,67 @@ planDaly100 = (pressUps, sitUps, squats) => {
     //let plan = {};
     //Number of sets needed for pressups
     targetReps = {};
+    sets = {};
     maxSets = 5;
-    let pressUpsSets = Math.floor(100/pressUps.reps.mid);
-    if (pressUpsSets === 0){
-        pressUpsSets = 1;
+    sets.pressUps = Math.floor(100/pressUps.reps.mid);
+    if (sets.pressUps === 0){
+        sets.pressUps = 1;
         targetReps.pressUps = pressUps.reps.mid;
         console.log(`Push yourself! Try to get more than ${targetReps.pressUps} Press ups in one set!`);
-    } else if (pressUpsSets < maxSets){
+    } else if (sets.pressUps < maxSets){
         let pressUpsBonusReps = 100 % pressUps.reps.mid;
         targetReps.pressUps = 100;
-        console.log(`${pressUpsSets} sets of ${pressUps.reps.mid} press ups and one set of ${pressUpsBonusReps}`);
+        console.log(`${sets.pressUps} sets of ${pressUps.reps.mid} press ups and one set of ${pressUpsBonusReps}`);
     } else {
-        pressUpsSets = maxSets;
-        targetReps.pressUps = pressUpsSets * pressUps.reps.mid;
-        console.log(`${pressUpsSets} sets of ${pressUps.reps.mid} press ups, for a total of ${targetReps.pressUps}. Continue final set to failure!`);
+        sets.pressUps = maxSets;
+        targetReps.pressUps = sets.pressUps * pressUps.reps.mid;
+        console.log(`${sets.pressUps} sets of ${pressUps.reps.mid} press ups, for a total of ${targetReps.pressUps}. Continue final set to failure!`);
     }
     //# Sets situps
-    let sitUpsSets = Math.floor(100/sitUps.reps.high);
-    if (sitUpsSets === 0){
-        sitUpsSets = 1;
+    sets.sitUps = Math.floor(100/sitUps.reps.high);
+    if (sets.sitUps === 0){
+        sets.sitUps = 1;
         targetReps.sitUps = sitUps.reps.high;
         console.log(`Push yourself! Try to get more than ${targetReps.sitUps} Sit ups in one set!`);
-    } else if (sitUpsSets < maxSets){
+    } else if (sets.sitUps < maxSets){
         let sitUpsBonusReps = 100 % sitUps.reps.high;
         targetReps.sitUps = 100;
-        console.log(`${sitUpsSets} sets of ${sitUps.reps.high} sit ups and one set of ${sitUpsBonusReps}`);
+        console.log(`${sets.sitUps} sets of ${sitUps.reps.high} sit ups and one set of ${sitUpsBonusReps}`);
     } else {
-        sitUpsSets = maxSets;
-        targetReps.sitUps = sitUpsSets * sitUps.reps.high;
-        console.log(`${sitUpsSets} sets of ${sitUps.reps.high} sit ups, for a total of ${targetReps.sitUps}. Continue final set to failure!`);
+        sets.sitUps = maxSets;
+        targetReps.sitUps = sets.sitUps * sitUps.reps.high;
+        console.log(`${sets.sitUps} sets of ${sitUps.reps.high} sit ups, for a total of ${targetReps.sitUps}. Continue final set to failure!`);
     }
     //# Sets Squats
-    let squatsSets = Math.floor(100/squats.reps.high);
-    if (squatsSets < maxSets){
+    sets.squats = Math.floor(100/squats.reps.high);
+    if (sets.squats < maxSets){
         let squatsBonusReps = 100 % squats.reps.high;
         targetReps.squats = 100;
-        console.log(`${squatsSets} sets of ${squats.reps.high} squats and one set of ${squatsBonusReps}`);
+        console.log(`${sets.squats} sets of ${squats.reps.high} squats and one set of ${squatsBonusReps}`);
     } else {
-        squatsSets = maxSets;
-        targetReps.squats = squatsSets * squats.reps.high;
-        console.log(`${squatsSets} sets of ${squats.reps.high} squats, for a total of ${targetReps.squats}. Continue final set to failure!`);
+        sets.squats = maxSets;
+        targetReps.squats = sets.squats * squats.reps.high;
+        console.log(`${sets.squats} sets of ${squats.reps.high} squats, for a total of ${targetReps.squats}. Continue final set to failure!`);
     }
+    return(targetReps, sets);
+}
+logResults = () => {
     //User input tbd
     console.log("Please enter how many reps you completed!")
-    let completedPressUps = 30; //make user input
+    let completedPressUps = 50; //make user input
     console.log(completedPressUps);
 
     let completedSitUps = 50; //make user input
     console.log(completedSitUps);
 
-    let completedSquats = 40; //make user input
+    let completedSquats = 50; //make user input
     console.log(completedSquats);
     //Plan adjustments (basic). On a second note, this method would be best for single set rep targets
     console.log(`BEFORE press up reps mid ${pressUps.reps.mid}, sit up reps high ${sitUps.reps.high}, squats reps high ${squats.reps.high}`)
-    pressUps.reps.mid += Math.floor(1+(completedPressUps-targetReps.pressUps) / (pressUpsSets + 2));
-    sitUps.reps.high += Math.floor(1+(completedSitUps-targetReps.sitUps) / (sitUpsSets + 2));
-    squats.reps.high += Math.floor(1+(completedSquats-targetReps.squats) / (squatsSets + 2));
+    pressUps.reps.mid += Math.floor(1+(completedPressUps-targetReps.pressUps) / (sets.pressUps + 2));
+    sitUps.reps.high += Math.floor(1+(completedSitUps-targetReps.sitUps) / (sets.sitUps + 2));
+    squats.reps.high += Math.floor(1+(completedSquats-targetReps.squats) / (sets.squats + 2));
     console.log(`AFTER press up reps mid ${pressUps.reps.mid}, sit up reps high ${sitUps.reps.high}, squats reps high ${squats.reps.high}`)
-    //requires save
 }
 planDaly100Save = (pressUps, sitUps, squats) => {
     localStorage.setItem("pressUps", JSON.stringify(pressUps));
@@ -73,7 +76,7 @@ loadLocalStorage = () => {
     pressUps = JSON.parse(localStorage.getItem("pressUps"));
     sitUps = JSON.parse(localStorage.getItem("sitUps"));
     squats = JSON.parse(localStorage.getItem("squats"));
-    console.log(pressUps)
-    console.log(sitUps)
-    console.log(squats)
+    console.log(pressUps);
+    console.log(sitUps);
+    console.log(squats);
 }
